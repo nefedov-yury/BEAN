c37171170839df17d5e9bafa2533ae6b3dd736fc
Nefedov Yury <nefedov.yury@jinr.ru>
2023-08-22 11:35:20 +0300

    picts for presentation

--- workdir/PsipJpsiPhiEta/ClonedTrks.cc
+++ workdir/PsipJpsiPhiEta/ClonedTrks.cc
@@ -106,8 +106,120 @@ void TrkDel(string fname, string lhead, string pdf) {
    leg->AddEntry(hr,"Ratio Cloned/All","LE");
    leg->Draw();
 
+   gPad->RedrawAxis();
+   c1->Update();
    if ( !pdf.empty() ) {
       c1->Print( pdf.c_str() );
+   }
+}
+
+// {{{1 plot angle
+//--------------------------------------------------------------------
+void ClnAng(string fname, string lhead, string pdf) {
+//--------------------------------------------------------------------
+   vector<TH1D*> hst = get_hists(fname, {"ct_ang"});
+
+   string nc = pdf.substr(4, pdf.find(".")-4);
+   TCanvas* c1 = new TCanvas(nc.c_str(),nc.c_str(),0,0,800,800);
+
+   // box to show selection region:
+   TBox* box = new TBox;
+   box->SetFillStyle(3001);
+   box->SetFillColor(kRed-10);
+   box->SetLineColor(kRed-10);
+   box->SetLineWidth(1);
+
+   c1->cd();
+   gPad->SetGrid();
+
+   hst[0]->SetTitle(";cos(#Theta);");
+   hst[0]->GetYaxis()->SetMaxDigits(3);
+   hst[0]->GetXaxis()->SetNdivisions(-405);
+   double ymax = 1.05 * hst[0]->GetMaximum();
+   hst[0]->SetMaximum(ymax);
+   hst[0]->SetLineWidth(2);
+   hst[0]->Draw("HIST");
+
+   box->DrawBox(0.9998,0.,1.,ymax);
+
+   TLegend* leg = new TLegend(0.15,0.78,0.60,0.88);
+   leg->AddEntry(hst[0],lhead.c_str(),"L");
+   leg->AddEntry(box,"Clone search area","F");
+   leg->Draw();
+
+   gPad->RedrawAxis();
+   c1->Update();
+   if ( !pdf.empty() ) {
+      c1->Print(pdf.c_str());
+   }
+}
+
+// {{{1 plot momentum distanse
+//--------------------------------------------------------------------
+void Cln_dP(string fname, string lhead, string pdf) {
+//--------------------------------------------------------------------
+   vector<TH1D*> hst = get_hists(fname, {"ct_dpp"});
+
+   string nc = pdf.substr(4, pdf.find(".")-4);
+   TCanvas* c1 = new TCanvas(nc.c_str(),nc.c_str(),0,0,800,800);
+
+   // box to show selection region:
+   TBox* box = new TBox;
+   box->SetFillStyle(3001);
+   box->SetFillColor(kRed-10);
+   box->SetLineColor(kRed-10);
+   box->SetLineWidth(1);
+
+   c1->cd();
+   gPad->SetGrid();
+
+   hst[0]->SetTitle(";momentum difference, GeV/c;");
+   hst[0]->GetYaxis()->SetMaxDigits(3);
+   double ymax = 1.05 * hst[0]->GetMaximum();
+   hst[0]->SetMaximum(ymax);
+   hst[0]->SetLineWidth(2);
+   hst[0]->Draw("HIST");
+
+   box->DrawBox(-0.005,0.,0.005,ymax);
+
+   TLegend* leg = new TLegend(0.54,0.79,0.89,0.89);
+   leg->AddEntry(hst[0],lhead.c_str(),"L");
+   leg->AddEntry(box,"Clone search area","F");
+   leg->Draw();
+
+   gPad->RedrawAxis();
+   c1->Update();
+   if ( !pdf.empty() ) {
+      c1->Print(pdf.c_str());
+   }
+}
+
+// {{{1 plot number found in event
+//--------------------------------------------------------------------
+void ClnNcl(string fname, string lhead, string pdf) {
+//--------------------------------------------------------------------
+   vector<TH1D*> hst = get_hists(fname, {"ct_ncl"});
+
+   string nc = pdf.substr(4, pdf.find(".")-4);
+   TCanvas* c1 = new TCanvas(nc.c_str(),nc.c_str(),0,0,800,800);
+
+   c1->cd();
+   gPad->SetGrid();
+   gPad->SetLogy(true);
+
+   hst[0]->SetTitle(";number of cloned tracks in one event;");
+   hst[0]->GetYaxis()->SetMaxDigits(3);
+   hst[0]->SetLineWidth(2);
+   hst[0]->Draw("HIST");
+
+   TLegend* leg = new TLegend(0.54,0.79,0.89,0.89);
+   leg->AddEntry(hst[0],lhead.c_str(),"L");
+   leg->Draw();
+
+   gPad->RedrawAxis();
+   c1->Update();
+   if ( !pdf.empty() ) {
+      c1->Print(pdf.c_str());
    }
 }
 
@@ -124,33 +236,47 @@ void ClonedTrks() {
    const string Dir = "prod_v709n3/";
    //========================================================
 
-   for ( int date : {2009,2012,2021} ) {
+   // for ( int date : {2009,2012,2021} ) {
+   for ( int date : {2021} ) {
       // for ( int type : { 0, 1, 2 } ) { // data, mcinc, mcsig
       for ( int type : { 0, 1 } ) {
          string fname, lhead, pdf;
+         string pdfA,pdfP,pdfN;
          switch ( type ) {
             case 0:
                fname = string(
                      Form("data_%02ipsip_all.root",date%100) );
                lhead = string( Form("Date %i",date) );
                pdf = string( Form("Cln_data_%02i.pdf",date%100) );
+               pdfA = string( Form("Ang_data_%02i.pdf",date%100) );
+               pdfP = string( Form("Mom_data_%02i.pdf",date%100) );
+               pdfN = string( Form("Ncl_data_%02i.pdf",date%100) );
                break;
             case 1:
                fname = string(
                      Form("mcinc_%02ipsip_all.root",date%100) );
                lhead = string( Form("MC inc. %i",date) );
                pdf = string( Form("Cln_mcinc_%02i.pdf",date%100) );
+               pdfA = string( Form("Ang_mcinc_%02i.pdf",date%100) );
+               pdfP = string( Form("Mom_mcinc_%02i.pdf",date%100) );
+               pdfN = string( Form("Ncl_mcinc_%02i.pdf",date%100) );
                break;
             case 2:
                fname = string(
                      Form("mcsig_kkmc_%02i.root",date%100) );
                lhead = string( Form("MC sig. %i",date) );
                pdf = string( Form("Cln_mcsig_%02i.pdf",date%100) );
+               pdfA = string( Form("Ang_mcsig_%02i.pdf",date%100) );
+               pdfP = string( Form("Mom_mcsig_%02i.pdf",date%100) );
+               pdfN = string( Form("Ncl_mcsig_%02i.pdf",date%100) );
                break;
          }
 
 
-         TrkDel(Dir+fname,lhead,pdf);
+         // TrkDel(Dir+fname,lhead,pdf);
+         // ClnAng(Dir+fname,lhead,pdfA);
+         // Cln_dP(Dir+fname,lhead,pdfP);
+         ClnNcl(Dir+fname,lhead,pdfN);
       }
    }
 
